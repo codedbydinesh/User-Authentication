ffe4bc4dcec6a23a306499e03f5a27ac734c44a4
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-08-29 07:47:29 +0530

    add verifyToken middleware for JWT authentication

---
+++ backend/src/middlewares/verifyToken.middleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+
+export const verifyToken = (req, res, next) => {
+    try {
+        let token = req.cookies.token || req.header("Authorization")?.replace("Bearer ","");
+        if(!token){
+            return res.status(401).json({message:"Unauthorized request"})    
+        }
+        
+        let decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY)
+
+        req.userId = decodedToken._id
+
+        next();
+
+
+    } catch (error) {
+        return res.status(500).json({message:"Server error"})
+    }
+}
