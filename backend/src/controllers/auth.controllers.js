fadec8519b3aa8f3deb528be215ec71c3d5a630a
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-08-29 07:53:10 +0530

    add getUserData function to retrieve user information by ID

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -122,3 +122,22 @@ export const logout = async (req, res) => {
     return res.status(500).json(error)
   }
 }
+
+export const getUserData = async (req, res) => {
+  try {
+    let userId = req.userId
+    if(!userId){
+      return res.status(400).json({message:"user is not found"})
+
+    }
+
+    let user = await User.findById(userId);
+    if(!user){
+      return res.status(400).json({message:"user is not found"})
+
+    }
+    return res.status(200).json(user)
+  } catch (error) {
+    return res.status(500).json({message:error})
+  }
+}
